640aaf20f02ca005b52c07bbfd19a9280b0c425e
Andrei <andreifrolov83@gmail.com>
2022-05-14 07:12:27 +0300

    feat: implement main component template

---
+++ src/components/MainItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="main">
+  </main>
+</template>
+
+<style scoped>
+.main {
+  grid-area: main;
+  line-height: 1.5;
+  width: 100vw;
+  height: 80vh;
+  background: linear-gradient(45deg, #2c3e50 0%, #1C1E1C 50%, #1C1E1C 51%, #2c3e50 100%);
+  display: grid;
+  grid-template-columns: 70% 30%;
+  border-top: 1px solid azure;
+  border-bottom: 1px solid azure;
+}
+</style>
