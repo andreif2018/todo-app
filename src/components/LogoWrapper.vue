32a48ed80417056d82b63e05e2a81a46047e20fe
Andrei <andreifrolov83@gmail.com>
2022-05-14 05:57:33 +0300

    feat: implement logo component

---
+++ src/components/LogoWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo-wrapper">
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="60"
+      height="60"
+    />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+}
+
+.logo-wrapper {
+  width: 30vw;
+  display: flex;
+  justify-content: center;
+}
+</style>
