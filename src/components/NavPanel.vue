ac1c5cf80de6993a37e7bc0bff03f9fc40a58195
Andrei <andreifrolov83@gmail.com>
2022-05-14 05:58:05 +0300

    feat: implement navigation component

---
+++ src/components/NavPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.nav-wrapper {
+  width: 30vw;
+  display: flex;
+  flex-direction: row;
+  place-items: flex-start;
+}
+</style>
