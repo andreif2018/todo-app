b0713adba2e3c2615a8a5a4c7809ab4fbf1af76e
Andrei <andreifrolov83@gmail.com>
2022-06-03 14:00:09 +0300

    refactor: update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,5 @@
 import type { Ref } from "vue";
 
-export const toolTipEnum = Object.freeze({
-  LENGTH: "Text length should be at least 2 characters",
-});
-
 export const toggleOn = (element: Ref) => {
   if (element.value === false) {
     element.value = !element.value;
@@ -14,10 +10,4 @@ export const toggleOff = (element: Ref) => {
   if (element.value === true) {
     element.value = !element.value;
   }
-};
-
-export default {
-  toolTipEnum,
-  toggleOn,
-  toggleOff,
 };
