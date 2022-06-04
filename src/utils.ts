6f9e353175c38a04a4c638729bf7dac4a93fa1d3
Andrei <andreifrolov83@gmail.com>
2022-06-04 10:41:54 +0300

    refactor: update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import type { Ref } from "vue";
+import { TextEnum } from "@/model";
 
 export const toggleOn = (element: Ref) => {
   if (element.value === false) {
@@ -9,5 +10,22 @@ export const toggleOn = (element: Ref) => {
 export const toggleOff = (element: Ref) => {
   if (element.value === true) {
     element.value = !element.value;
+  }
+};
+
+export const validateInput = (
+  inputText: string | undefined,
+  minHint: Ref,
+  MaxHint: Ref
+) => {
+  if (inputText && inputText.length > TextEnum.MAX_LENGTH) {
+    toggleOff(minHint);
+    toggleOn(MaxHint);
+  } else if (inputText && inputText.length >= TextEnum.MIN_LENGTH) {
+    toggleOff(MaxHint);
+    toggleOff(minHint);
+  } else {
+    toggleOff(MaxHint);
+    toggleOn(minHint);
   }
 };
