90587c8dc8236a9e4da28b861689cee4a45b8f1b
Andrei <andreifrolov83@gmail.com>
2022-06-11 19:30:08 +0300

    test: AddToDo.spec.ts

---
+++ src/components/__tests__/AddToDo.spec.ts
@@ -0,0 +1,62 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import AddToDo from "../AddToDo.vue";
+
+describe("CheckAddToDoBox", () => {
+  const wrapper = mount(AddToDo);
+  const container = wrapper.find(".add-container");
+  const button = wrapper.find(".add-button");
+  const textField = wrapper.find(".title");
+
+  it("renders proper component tag name", () => {
+    expect(wrapper.element.tagName).toEqual("DIV");
+  });
+
+  it("assigns proper css class", () => {
+    expect(wrapper.classes()).toEqual(["add-wrapper"]);
+  });
+
+  it("renders child element ToolTip", () => {
+    expect(wrapper.findComponent("ToolTip")).toBeTruthy();
+  });
+
+  it("renders proper child container structure", () => {
+    expect(container.element.childElementCount).toBe(2);
+  });
+
+  it("renders proper child container tag name", () => {
+    expect(container.element.tagName).toEqual("DIV");
+  });
+
+  it("renders proper button tag name", () => {
+    expect(button.element.tagName).toEqual("BUTTON");
+  });
+
+  it("assigns proper css class to button element", () => {
+    expect(button.classes()).toEqual(["add-button"]);
+  });
+
+  it("renders proper input text tag name", () => {
+    expect(textField.element.tagName).toEqual("INPUT");
+  });
+
+  it("assigns proper css class to input text element", () => {
+    expect(textField.classes()).toEqual(["title"]);
+  });
+
+  it("assigns proper attribute to input text element", () => {
+    expect(textField.attributes("type")).toEqual("text");
+  });
+
+  it("assigns proper attribute to input text element", () => {
+    expect(textField.attributes("placeholder")).toEqual("Type Todo here...");
+  });
+
+  // it("sensitive to input text", async () => {
+  //   const data = "test data";
+  //   await textField.setValue(data);
+  //   // await button.trigger("click");
+  //   console.log(textField.text());
+  //   expect(textField.text()).toBe("qwe");
+  // });
+});
