3eac45500302e7c1caaabf7b0a14d4161fcd8b93
Andrei <andreifrolov83@gmail.com>
2022-06-11 11:55:08 +0300

    test: HeaderItem.spec.ts

---
+++ src/components/__tests__/HeaderItem.spec.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import HeaderItem from "../HeaderItem.vue";
+
+describe("HeaderItem", () => {
+  const testProps = "test header";
+  const wrapper = mount(HeaderItem, { props: { msg: testProps } });
+  const childDiv = wrapper.find(".title");
+
+  it("renders proper component tag name", () => {
+    expect(wrapper.element.tagName).toEqual("HEADER");
+  });
+
+  it("assigns proper css class", () => {
+    expect(wrapper.classes()).toEqual(["header"]);
+  });
+
+  it("renders proper props", () => {
+    expect(childDiv.find("H1").text()).toEqual(testProps);
+  });
+
+  it("renders proper child element tag name", () => {
+    expect(childDiv.element.tagName).toEqual("DIV");
+  });
+
+  it("renders proper component structure", () => {
+    expect(wrapper.element.childElementCount).toBe(3);
+  });
+
+  it("renders child element LogoItem", () => {
+    expect(wrapper.findComponent("LogoItem")).toBeTruthy();
+  });
+
+  it("renders child element LogoItem", () => {
+    expect(wrapper.findComponent("NavPanel")).toBeTruthy();
+  });
+});
