b5d3bce8d7413e2a0d147dfae0d8f3cae11b2dba
Andrei <andreifrolov83@gmail.com>
2022-06-11 16:23:08 +0300

    test: CheckBox.spec.ts

---
+++ src/components/__tests__/CheckBox.spec.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import CheckBox from "../CheckBox.vue";
+
+describe("CheckBox", () => {
+  const wrapper = mount(CheckBox, { props: { msg: false } });
+  const checkBox = wrapper.find(".customCheck");
+
+  it("renders proper component tag name", () => {
+    expect(wrapper.element.tagName).toEqual("INPUT");
+  });
+
+  it("assigns proper css class", () => {
+    expect(wrapper.classes()).toEqual(["customCheck"]);
+  });
+
+  it("renders proper attribute type", () => {
+    expect(wrapper.attributes("type")).toEqual("checkbox");
+  });
+
+  it("renders proper component structure", () => {
+    expect(wrapper.element.childElementCount).toBe(0);
+  });
+
+  it("renders proper default attribute", () => {
+    expect(checkBox.attributes("checked")).toBeFalsy();
+  });
+
+  it("renders proper default props", () => {
+    expect(wrapper.props().isChecked).toBe(false);
+  });
+
+  it("sensitive to checking checkbox", async () => {
+    await checkBox.trigger("click");
+    expect(wrapper.emitted()).toHaveProperty("check");
+  });
+
+  it("renders updated props", async () => {
+    await wrapper.setProps({ isChecked: true });
+    expect(wrapper.props().isChecked).toBe(true);
+  });
+
+  it("sensitive to unchecking checkbox", async () => {
+    await checkBox.trigger("click");
+    expect(wrapper.emitted()).toHaveProperty("check", [[true], [false]]);
+  });
+});
