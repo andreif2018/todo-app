222161c5f77a7d6ffbfab9f55311e7baa7c4e228
Andrei <andreifrolov83@gmail.com>
2022-06-16 15:58:37 +0300

    test: HomeView.spec.ts

---
+++ src/components/__tests__/HomeView.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import HomeView from "../../views/HomeView.vue";
+import ToDoList from "../ToDoList.vue";
+import FooterItem from "../FooterItem.vue";
+import HeaderItem from "../HeaderItem.vue";
+
+describe("Check HomeView page", async () => {
+  const wrapper = mount(HomeView);
+
+  it("renders proper component tag name", () => {
+    expect(wrapper.element.tagName).toEqual("DIV");
+  });
+
+  it("assigns proper css class", () => {
+    expect(wrapper.classes()).toEqual(["home"]);
+  });
+
+  it("renders child element HeaderItem", () => {
+    expect(wrapper.findComponent(HeaderItem).exists()).toBeTruthy();
+  });
+
+  it("renders child element ToDoList", () => {
+    expect(wrapper.findComponent(ToDoList).exists()).toBeTruthy();
+  });
+
+  it("renders child element FooterItem", () => {
+    expect(wrapper.findComponent(FooterItem).exists()).toBeTruthy();
+  });
+
+  it("renders child element main", () => {
+    expect(wrapper.find("main").classes()).toEqual(["main"]);
+  });
+
+  it("renders proper child element count", () => {
+    expect(wrapper.element.childElementCount).toEqual(3);
+  });
+});
