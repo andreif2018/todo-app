cd85fbe20e4ad1dfdae7f781d692276ca186e612
Andrei <andreifrolov83@gmail.com>
2022-06-18 18:42:55 +0300

    test: ModalPopup.spec.ts

---
+++ src/components/__tests__/ModalPopup.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import ModalPopup from "../ModalPopup.vue";
+import { TestEnum } from "./test-model";
+
+describe("Check ToDoItem Component", async () => {
+  const wrapper = mount(ModalPopup, {
+    props: {
+      msg: {
+        todo: "item",
+        created: "time",
+        modified: "time 2",
+        completed: "time 3",
+      },
+    },
+  });
+  const messageContainer = wrapper.find(".message-wrapper");
+
+  it("renders component tag name", () => {
+    expect(wrapper.element.tagName).toEqual(TestEnum.DIV);
+  });
+
+  it("assigns css class", () => {
+    expect(wrapper.classes()).toEqual(["modal"]);
+  });
+
+  it("render child elements count", () => {
+    expect(wrapper.element.childElementCount).toBe(2);
+  });
+
+  it("render message-wrapper", () => {
+    expect(messageContainer.element.tagName).toEqual(TestEnum.DIV);
+  });
+
+  it("render message-wrapper text", () => {
+    expect(messageContainer.element.textContent).toEqual(
+      "Todo: itemCreated: timeModified: time 2Completed: time 3"
+    );
+  });
+});
