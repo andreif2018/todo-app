94c77ece73237d8459c812889d0a14c370fbfe62
Andrei <andreifrolov83@gmail.com>
2022-06-21 16:08:26 +0300

    test: implement store.spec.ts

---
+++ src/components/__tests__/store.spec.ts
@@ -0,0 +1,116 @@
+import { setActivePinia, createPinia } from "pinia";
+import { describe, it, expect, beforeEach } from "vitest";
+import { useToDoStore } from "@/stores/todos";
+
+describe("Todo Store", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+  const testTodoName = "I am test ToDo";
+
+  it("check default state", () => {
+    const store = useToDoStore();
+    expect(store.todos).toStrictEqual([]);
+  });
+
+  it("adding new item", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    expect(store.todos.length).toBe(1);
+    expect(store.todos[0].todoName).toEqual(testTodoName);
+    expect(store.todos[0].modifiedTime).toBeFalsy();
+    expect(store.todos[0].completedTime).toBeFalsy();
+    expect(store.todos[0].isDone).toBeFalsy();
+  });
+
+  it("clear all", () => {
+    const store = useToDoStore();
+    store.createNewItem("1st item");
+    expect(store.todos.length).toBe(1);
+    store.clearAll();
+    expect(store.todos).toStrictEqual([]);
+    store.createNewItem("new 1st item");
+    store.createNewItem("2nd item");
+    expect(store.todos.length).toBe(2);
+    store.clearAll();
+    expect(store.todos).toStrictEqual([]);
+  });
+
+  it("update item", () => {
+    const store = useToDoStore();
+    store.createNewItem("1st item");
+    const todoID = store.todos[0]._id;
+    store.updateItem(todoID, testTodoName);
+    expect(store.todos.length).toBe(1);
+    expect(store.todos[0].todoName).toEqual(testTodoName);
+    expect(store.todos[0].modifiedTime).toBeTruthy();
+    expect(store.todos[0].completedTime).toBeFalsy();
+    expect(store.todos[0].isDone).toBeFalsy();
+  });
+
+  it("check item", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    const todoID = store.todos[0]._id;
+    store.checkToDo(todoID);
+    expect(store.todos.length).toBe(1);
+    expect(store.todos[0].todoName).toEqual(testTodoName);
+    expect(store.todos[0].modifiedTime).toBeFalsy();
+    expect(store.todos[0].completedTime).toBeTruthy();
+    expect(store.todos[0].isDone).toBeTruthy();
+  });
+
+  it("uncheck item", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    const todoID = store.todos[0]._id;
+    store.checkToDo(todoID);
+    expect(store.todos[0].completedTime).toBeTruthy();
+    expect(store.todos[0].isDone).toBeTruthy();
+    store.checkToDo(todoID);
+    expect(store.todos[0].completedTime).toBeFalsy();
+    expect(store.todos[0].isDone).toBeFalsy();
+  });
+
+  it("delete item", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    const todoID = store.todos[0]._id;
+    store.deleteItem(todoID);
+    expect(store.todos.length).toBe(0);
+  });
+
+  it("method createNewItem without argument", () => {
+    const store = useToDoStore();
+    store.createNewItem("");
+    expect(store.todos.length).toBe(0);
+  });
+
+  it("method delete with incorrect argument", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    expect(store.todos.length).toBe(1);
+    const todoID = store.todos[0]._id;
+    store.deleteItem(todoID.slice(-1));
+    expect(store.todos.length).toBe(1);
+  });
+
+  it("method updateItem with incorrect argument", () => {
+    const store = useToDoStore();
+    store.createNewItem(testTodoName);
+    expect(store.todos[0].todoName).toEqual(testTodoName);
+    const todoID = store.todos[0]._id;
+    store.updateItem(todoID.slice(-1), "new todo name");
+    expect(store.todos[store.findIndexById(todoID)].todoName).toEqual(
+      testTodoName
+    );
+    store.updateItem(todoID, "");
+    expect(store.todos[store.findIndexById(todoID)].todoName).toEqual(
+      testTodoName
+    );
+    store.updateItem(todoID.slice(-1), "");
+    expect(store.todos[store.findIndexById(todoID)].todoName).toEqual(
+      testTodoName
+    );
+  });
+});
