5c47b8afb43ac3b59a65c293b28bbcdaf0ada976
Andrei <andreifrolov83@gmail.com>
2022-06-23 11:57:25 +0300

    feat: implement TabItem.vue

---
+++ src/components/TabItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toggleOff, toggleOn } from "@/utils/utils";
+import { ref } from "vue";
+const props = defineProps<{
+  msg: string;
+  isChecked: boolean;
+  left?: boolean;
+}>();
+const isCheckedRef = ref(props.isChecked);
+const handleCheck = () => {
+  isCheckedRef.value ? toggleOff(isCheckedRef) : toggleOn(isCheckedRef);
+};
+</script>
+
+<template>
+  <input
+    class="custom-check"
+    type="checkbox"
+    @click="handleCheck"
+    :checked="isCheckedRef"
+  />
+</template>
+
+<style scoped></style>
