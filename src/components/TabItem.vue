48c06f53675bc3b037377a5b8cbc8e2f07ed70ed
Andrei <andreifrolov83@gmail.com>
2022-06-25 17:36:56 +0300

    refactor: update TabItem.vue

--- src/components/TabItem.vue
+++ src/components/TabItem.vue
@@ -24,8 +24,8 @@ const handleCheck = () => {
     :class="{ checked: isCheckedRef, left: props.left, right: props.right }"
     @click="handleCheck"
   >
-    <label v-if="!isCheckedRef" for="tab">Hide<br />{{ props.msg }}</label>
-    <label v-else for="tab">Show<br />{{ props.msg }}</label>
+    <label v-if="!isCheckedRef" for="tab">Show<br />{{ props.msg }}</label>
+    <label v-else for="tab">Hide<br />{{ props.msg }}</label>
     <input
       class="custom-tab"
       type="checkbox"
