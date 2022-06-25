dc67a560b7874ae531b28f703eb819c1f4e05821
Andrei <andreifrolov83@gmail.com>
2022-06-25 06:21:23 +0300

    test: implement FilterItem.spec.ts

---
+++ src/components/__tests__/FilterItem.spec.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect, vitest } from "vitest";
+import { mount } from "@vue/test-utils";
+import FilterItem from "../FilterItem.vue";
+import TabItem from "../TabItem.vue";
+import { TestEnum } from "./model/test-model";
+import { createTestingPinia } from "@pinia/testing";
+
+describe("FilterItem", () => {
+  const wrapper = mount(FilterItem, {
+    global: {
+      plugins: [
+        createTestingPinia({
+          createSpy: vitest.fn,
+        }),
+      ],
+    },
+  });
+
+  const filterTitle = wrapper.find(".status-filters-title");
+  const tabContainer = wrapper.find(".box-wrapper");
+
+  it("renders component tag name", () => {
+    expect(wrapper.element.tagName).toEqual(TestEnum.DIV);
+  });
+
+  it("assigns css class name", () => {
+    expect(wrapper.element.className).toEqual("filter-container");
+  });
+
+  it("renders component structure", () => {
+    expect(wrapper.element.childElementCount).toBe(2);
+  });
+
+  it("renders child element TabItem", () => {
+    expect(wrapper.findComponent(TabItem).exists()).toBe(true);
+  });
+
+  it("renders 1st child element tag name", () => {
+    expect(filterTitle.element.tagName).toEqual("P");
+  });
+
+  it("renders 2nd child element linked text", () => {
+    expect(tabContainer.element.tagName).toEqual(TestEnum.DIV);
+  });
+
+  it("renders 2nd child element structure", () => {
+    expect(tabContainer.element.childElementCount).toBe(2);
+  });
+});
