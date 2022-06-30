aa65dd6fef47e3222eca66e189406b8e05ac1476
Andrei <andreifrolov83@gmail.com>
2022-06-30 08:59:01 +0300

    refactor: update ModalPopup.vue

--- src/components/ModalPopup.vue
+++ src/components/ModalPopup.vue
@@ -55,7 +55,7 @@ const props = defineProps<{
   align-items: center;
   position: absolute;
   top: -50%;
-  left: 0;
+  right: calc(var(--width-work-area) / 2);
   z-index: 1000;
   height: max-content;
 }
